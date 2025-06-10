a81d48c8e5bae211b54547992ed08f632f54750b
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-06-10 10:47:32 -0500

    haciendo las segunda consulta de ala tabla Contactos de solution 000

--- Solution-000/Form1.cs
+++ Solution-000/Form1.cs
@@ -21,14 +21,39 @@ namespace Solution_000
                     conexion.Open(); // abrimos la conexion
                     string consulta = "INSERT INTO Contactos (Nombre,Telefono) values (@nombre,@telefono)";
                     MySqlCommand comando = new MySqlCommand(consulta, conexion);
-                    
-                    comando.Parameters.AddWithValue("@nombre",txtNombre.Text); // los campos se agregan alas consultas en el insert 
+
+                    comando.Parameters.AddWithValue("@nombre", txtNombre.Text); // los campos se agregan alas consultas en el insert 
                     comando.Parameters.AddWithValue("@telefono", txtTelefono.Text); // los campos se agregan alas consultas en el insert 
 
                     comando.ExecuteNonQuery(); // metodo que crea modifica y elemina tablas 
 
                     MessageBox.Show("Contacto Guardado"); // mostramos un mensaje que nos muestra si este se envio ala base de datos 
 
+                    txtNombre.Clear();
+                    txtTelefono.Clear();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errro" + ex.Message);
+                }
+            }
+            using (MySqlConnection conexion = new MySqlConnection())
+            {
+                listView1.Items.Clear();
+                try
+                {
+                    conexion.Open();
+                    string consulta = "SELECT * FROM Contactos";
+                    MySqlCommand cmd = new MySqlCommand(consulta,conexion); // creamos un obejto para hacer las consultas 
+                    MySqlDataReader raeeder = cmd.ExecuteReader(); // crea la conexion y manda este objeto ala base de datos  ara este crear la consulta
+                    // selecionar los datos y pasarlo por el el Metodo ExecuteReader();
+
+                    // usamos un while
+                    while (raeeder.Read()) // Read es Leer reader lo que capturamos del metodo los datos y mandarlos a esta lista si se cumple 
+                    {
+
+                    }
                 }
                 catch (Exception ex)
                 {
