e32ef689fead9676862b3589bbde5ff7dec87279
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-06-10 10:04:53 -0500

    creamos las tablas con su respectiva base de datos

--- Solution-000/obj/Debug/net8.0-windows/Solution-000.AssemblyInfo.cs
+++ Solution-000/obj/Debug/net8.0-windows/Solution-000.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Solution-001")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Solution-000")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f2005ceea1fa1cdfb2a971da5ba793a9ec4d07")]
-[assembly: System.Reflection.AssemblyProductAttribute("Solution-001")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Solution-001")]
+[assembly: System.Reflection.AssemblyProductAttribute("Solution-000")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Solution-000")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
---
+++ Tablas-000.sql
@@ -0,0 +1,5 @@
+create table Contactos (
+ID int primary KEY auto_increment,
+Nombre varchar(50),
+telefono varchar(20)
+);
