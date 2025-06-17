88bb41f61a9244d87f12a4a99d1ab062388f8c4f
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-06-17 09:37:38 -0500

    gestion de ventas  frotend listo

--- solution-002/Form1.Designer.cs
+++ solution-002/Form1.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaptionText;
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(59, 90);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(425, 216);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ButtonHighlight;
+            panel2.BorderStyle = BorderStyle.Fixed3D;
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label2);
+            panel2.Location = new Point(7, 11);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(406, 190);
+            panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Showcard Gothic", 18F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(197, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(143, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Cine Bomm";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Showcard Gothic", 14.25F, FontStyle.Italic);
+            label2.Location = new Point(22, 18);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 23);
+            label2.TabIndex = 0;
+            label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Showcard Gothic", 14.25F, FontStyle.Italic);
+            label3.Location = new Point(22, 59);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 23);
+            label3.TabIndex = 1;
+            label3.Text = "Correo";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(127, 18);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(198, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(127, 59);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(198, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Showcard Gothic", 14.25F, FontStyle.Italic);
+            button1.Location = new Point(113, 120);
+            button1.Name = "button1";
+            button1.Size = new Size(157, 46);
+            button1.TabIndex = 4;
+            button1.Text = "Registrate";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(529, 568);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button button1;
     }
 }
